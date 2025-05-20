944b90260cf4e0feab0deaa9123376ae2660b976
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-05-19 21:02:50 -0500

    fix :bug: mistakes correction

--- script/index.js
+++ script/index.js
@@ -1,16 +1,14 @@
 console.log("holiiiiiiiiiiii")
 
-let usuario = document.getElementById("user").value;
-let contraseña = document.getElementById("password").value;
 const nextPage = document.getElementById("boton");
 nextPage.addEventListener("click", function() {
     usuario = document.getElementById("user").value;
     contraseña = document.getElementById("password").value;
     if (usuario === "Usuario" && contraseña === "campus2023") {
-        window.locationhref = "../html/Usuario/paginaPrincipal.html"; 
+        window.location.href = "../html/Usuario/paginaPrincipal.html"; 
     }
     else if(usuario === "Administrador" && contraseña === "campus2023") {
-        window.locationhref = "../html/admin/paginaPrincipal.html"; 
+        window.location.href = "../html/admin/paginaPrincipal.html"; 
     } 
     else {
         let mensaje = document.getElementById("mesaje");
