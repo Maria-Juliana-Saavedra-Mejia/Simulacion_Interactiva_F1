084ecc04fc7633b58cc21f5dbd1ff2ef1df9f83c
Maria-Juliana-Saavedra-Mejia <saavedra.mariaju@gmail.com>
2025-05-19 20:59:04 -0500

    fix :bug: mistakes correction

--- script/index.js
+++ script/index.js
@@ -7,10 +7,10 @@ nextPage.addEventListener("click", function() {
     usuario = document.getElementById("user").value;
     contraseña = document.getElementById("password").value;
     if (usuario === "Usuario" && contraseña === "campus2023") {
-        window.location.href = "../html/Usuario/paginaPrincipal.html"; 
+        window.locationhref = "../html/Usuario/paginaPrincipal.html"; 
     }
     else if(usuario === "Administrador" && contraseña === "campus2023") {
-        window.location.href = "../html/admin/paginaPrincipal.html"; 
+        window.locationhref = "../html/admin/paginaPrincipal.html"; 
     } 
     else {
         let mensaje = document.getElementById("mesaje");
